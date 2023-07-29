55a0ea2f7481d3067d1e620357b5adb4a2e3265d
Alexandros Sklavounos <alex_sklavounos@yahoo.gr>
2023-07-28 22:50:32 -0400

    Fix file mess

--- docs/history.rst
+++
@@ -1,8 +0,0 @@
-:tocdepth: 1
-
-.. _changes:
-
-History
-*******
-
-.. include:: ../NEWS (links).rst
--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
+import sys
 from setuptools import setup
 
-import sys
 sys.path.insert(0, '.')
 import versioneer
 
 setup(name='path_helpers',
-         version=versioneer.get_version(),
-         cmdclass=versioneer.get_cmdclass(),
-         description='Helper class and functions for working with file path',
-         author='Christian Fobel',
-         author_email='christian@fobel.net',
-         url='http://github.com/cfobel/path_helpers',
-         license='MIT License',
-         packages=['path_helpers'],
-         classifiers=
-         ['Development Status :: 5 - Production/Stable',
-          'License :: OSI Approved :: MIT License',
-          'Intended Audience :: Developers',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Topic :: Software Development :: Libraries :: Python Modules'])
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      description='Helper class and functions for working with file path',
+      author='Christian Fobel',
+      author_email='christian@fobel.net',
+      url='http://github.com/cfobel/path_helpers',
+      license='MIT License',
+      packages=['path_helpers'],
+      classifiers=
+      ['Development Status :: 5 - Production/Stable',
+       'License :: OSI Approved :: MIT License',
+       'Intended Audience :: Developers',
+       'Operating System :: OS Independent',
+       'Programming Language :: Python',
+       'Topic :: Software Development :: Libraries :: Python Modules'])
